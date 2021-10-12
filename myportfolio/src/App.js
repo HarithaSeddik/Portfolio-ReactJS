4ee3ae2442e80077439f854092e305a9f6b0c4dd
Haritha <akkad.haritha@gmail.com>
2021-10-12 22:29:53 +0300

    add homesection, removed experiences section

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,37 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-
+import HomeSection from "./component/HomeSection";
 import Navbar from "./component/Navbar";
 import jsonData from "./data/data.json";
-import { Parallax } from "react-parallax";
-import { ExternalLink } from "react-external-link";
-function App() {
-  const inlineStyle = {
-    // background: '#fff',
-    background: "transparent",
-    color: "white",
-    left: "50%",
-    top: "65%",
-    position: "absolute",
-    padding: "20px",
-    transform: "translate(-50%, -50%)",
-    // fontSize:'40px',
-    alignContent: "center",
-    textAlign: "center",
-    textTransform: "uppercase",
-    verticalAlign: "middle",
-    lineHeight: "20px",
-    fontWeight: "100",
-    letterSpacing: ".15em",
-    whiteSpace: "nowrap",
-  };
 
+function App() {
+  
   const [snapScroll, setSnapScroll] = useState(true);
   return (
     <div className="App">
       <Navbar />
       <section id="home">
-        {/* <HomeSection /> */}
+        <HomeSection />
       </section>
      
     </div>
