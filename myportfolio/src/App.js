838961ca21ddd56cdef3d19b944a066e503f21ee
Haritha <akkad.haritha@gmail.com>
2021-10-12 23:36:48 +0300

    added cursor component to main

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import HomeSection from "./component/HomeSection";
 import Navbar from './component/Navbar';
+import Cursor from './component/Cursor';
 function App() {
   return (
     <div className="App">
+      <Cursor/>
       <Navbar/>
       <section
             id="home">
