a598c1becd588755e38f97854660796bd803895b
Haritha <akkad.haritha@gmail.com>
2021-10-12 23:20:07 +0300

    added navbar to main, but removed javascript functions

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import HomeSection from "./component/HomeSection";
+import Navbar from './component/Navbar';
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       <section
             id="home">
             <HomeSection />
