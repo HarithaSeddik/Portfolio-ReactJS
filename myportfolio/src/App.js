397389771f12d050c6a31090f38fb297fceade49
Haritha <akkad.haritha@gmail.com>
2021-10-12 22:31:37 +0300

    deleted navbar files

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import HomeSection from "./component/HomeSection";
-import Navbar from "./component/Navbar";
+
 import jsonData from "./data/data.json";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   const [snapScroll, setSnapScroll] = useState(true);
   return (
     <div className="App">
-      <Navbar />
+
       <section id="home">
         <HomeSection />
       </section>
