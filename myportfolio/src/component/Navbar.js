ceb6bf29b279132b23ac1064f8dc0ada7fcc03bb
Haritha <akkad.haritha@gmail.com>
2021-10-13 15:31:48 +0300

    edited navbar active class

--- myportfolio/src/component/Navbar.js
+++ myportfolio/src/component/Navbar.js
@@ -4,12 +4,12 @@ import {Link} from 'react-scroll'
 import { useEffect } from 'react/cjs/react.development';
 import { faLeaf } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
+const Navbar = (props) => {
 
     const sections = document.querySelectorAll("section");
     const navLi = document.querySelectorAll(".navbarContainer .textContainer ul li");
     const navbarContainer =  document.querySelectorAll(".navbarContainer");
-    const [showBackground, setShowBackground] = useState(false)
+    // const [showBackground, setShowBackground] = useState(false)
 
     // const [showBackground, setShowBackground] = useState(false)
     
@@ -39,9 +39,9 @@ const Navbar = () => {
 
     return (
        
-         <div className={showBackground?'navbarContainer--active':'navbarContainer'}> 
-                {/* {console.log(showBackground)} */}
-            <div className={showBackground?'textContainer--active':'textContainer'}>
+         <div className={props.showBackground?'navbarContainer--active':'navbarContainer'}> 
+                {console.log(props.showBackground)}
+            <div className={props.showBackground?'textContainer--active':'textContainer'}>
                 <ul className='textList'>
                     <Link onClick={handleClick} smooth to='home'>
                         <li className='home'>  Home </li>
