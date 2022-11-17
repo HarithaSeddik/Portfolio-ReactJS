15bbb9b4b3b0d95b61106180350a03f1222252b2
Harisa Seddik <harisa.seddik@kobil.com>
2022-11-17 20:24:42 +0300

    now home section shows active in navbar

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 
-import  { useEffect, useState } from "react";
-
-
-
+import { useEffect, useState } from "react";
 
 import Media from "react-media";
 
@@ -17,52 +14,48 @@ import HomeSection from "./component/page_sections/home_section/HomeSection";
 import AboutSection from "./component/page_sections/about_section/AboutSection";
 import Skills from "./component/page_sections/skills_section/Skills";
 
-
-
-
 function App() {
   const [showButton, setShowButton] = useState(false);
   const [showNavBackground, setShowNavBackground] = useState(null);
-  
+
   const bodyElement = document.getElementsByTagName("BODY")[0];
   const htmlElement = document.getElementsByTagName("HTML")[0];
   const sections = document.querySelectorAll("section");
-  const navLi = document.querySelectorAll(".navbarContainer .textContainer ul li");
-  
+  const navLi = document.querySelectorAll(
+    ".navbarContainer .textContainer ul li"
+  );
 
   useEffect(() => {
     function handleScroll() {
-      
-      let current = ''
-      
+      let current = "";
+
       if (window.scrollY < 100) {
         setShowButton(false);
         setShowNavBackground(false);
-        
       } else if (window.scrollY < window.innerHeight / 2) {
         setShowNavBackground(false);
       } else {
         setShowButton(true);
         setShowNavBackground(true);
-        
       }
 
-      sections.forEach( (section)=> {
+      sections.forEach((section) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        
-        if (window.scrollY >= (sectionTop- 0.1*sectionTop) && window.scrollY <= ( sectionTop + sectionHeight ) ){
-          current = section.getAttribute('id')
-          
+
+        if (
+          window.scrollY >= sectionTop - 0.1 * sectionTop &&
+          window.scrollY <= sectionTop + sectionHeight
+        ) {
+          current = section.getAttribute("id") ?? "home";
         }
-      })
+      });
       navLi.forEach((li) => {
         li.classList.remove("active");
         if (li.classList.contains(current)) {
           li.classList.add("active");
         }
       });
-
     }
     window.addEventListener("scroll", handleScroll, { passive: true });
     return () => window.removeEventListener("scroll", handleScroll);
@@ -78,57 +71,60 @@ function App() {
 
   return (
     <>
-    <Cursor/>
-    <Media queries = {{
-       small: "(max-width: 599px)",
-       medium: "(min-width: 600px) and (max-width: 1199px)",
-       large: "(min-width: 1200px)"
-    }}>
-    {matches => ( 
-      <>
-      {matches.small && <DisabledSection/> }
-      {matches.medium && <DisabledSection/>}
-      {matches.large && <div className='App'>
-        
-    <div
-          id="navbar"
-          style={{
-            top: "0%",
-            position: "fixed",
-            zIndex: "999",
-            width: "100%",
-            height: "8vh",
-          }}
-          >
-          <Navbar showBackground={showNavBackground} />
-        </div>
-        {showButton && (
-          <div onClick={handleTopClick}>
-            <BackToTop />
-          </div>
+      <Cursor />
+      <Media
+        queries={{
+          small: "(max-width: 599px)",
+          medium: "(min-width: 600px) and (max-width: 1199px)",
+          large: "(min-width: 1200px)",
+        }}
+      >
+        {(matches) => (
+          <>
+            {matches.small && <DisabledSection />}
+            {matches.medium && <DisabledSection />}
+            {matches.large && (
+              <div className="App">
+                <div
+                  id="navbar"
+                  style={{
+                    top: "0%",
+                    position: "fixed",
+                    zIndex: "999",
+                    width: "100%",
+                    height: "8vh",
+                  }}
+                >
+                  <Navbar showBackground={showNavBackground} />
+                </div>
+                {showButton && (
+                  <div onClick={handleTopClick}>
+                    <BackToTop />
+                  </div>
+                )}
+                <section id="home" style={{ scrollSnapAlign: "start" }}>
+                  <HomeSection />
+                </section>
+                <section id="aboutMe" style={{ scrollSnapAlign: "start" }}>
+                  <AboutSection />
+                </section>
+                <section id="experiences" style={{ scrollSnapAlign: "start" }}>
+                  <ExperiencesSection />
+                </section>
+
+                <section id="skills" style={{ scrollSnapAlign: "start" }}>
+                  <Skills />
+                </section>
+
+                <section id="contact" style={{ scrollSnapAlign: "start" }}>
+                  <Contact />
+                </section>
+              </div>
+            )}
+          </>
         )}
-         <section id="home" style={{ scrollSnapAlign: "start" }}>
-          <HomeSection />
-        </section>
-        <section id="aboutMe" style={{ scrollSnapAlign: "start" }}>
-          <AboutSection />
-        </section>
-        <section id="experiences" className="experiencesContainer">
-          <ExperiencesSection />
-        </section>
-
-        <section id="skills" style={{ scrollSnapAlign: "start" }}>
-          <Skills />
-        </section>
-
-        <section id="contact" style={{ scrollSnapAlign: "start" }}>
-          <Contact />
-        </section>
-        </div>}
-      </>
-    )}
-        </Media>
-    </> 
+      </Media>
+    </>
   );
 }
 
